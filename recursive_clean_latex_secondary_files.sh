d0a2b003359a512bce6d21a79c5119a7f355bea7
elisa-aleman <elisa.claire.aleman.carreon@gmail.com>
2022-06-08 10:24:04 +0900

    Added more files to trash

--- recursive_clean_latex_secondary_files.sh
+++ recursive_clean_latex_secondary_files.sh
@@ -26,3 +26,16 @@ find . -name '*.lof' -type f -delete
 find . -name '*.lot' -type f -delete
 find . -name '*.run.xml' -type f -delete
 find . -name '*-blx.bib' -type f -delete
+find . -name '*.maf' -type f -delete
+find . -name '*.mtc' -type f -delete
+find . -name '*.mtc0' -type f -delete
+find . -name '*.mtc1' -type f -delete
+find . -name '*.mtc2' -type f -delete
+find . -name '*.mtc3' -type f -delete
+find . -name '*.mtc4' -type f -delete
+find . -name '*.mtc5' -type f -delete
+find . -name '*.mtc6' -type f -delete
+find . -name '*.mtc7' -type f -delete
+find . -name '*.mtc8' -type f -delete
+find . -name '*.mtc9' -type f -delete
+find . -name '*.bcf' -type f -delete
